cc493fb8947f0d8d45bfba1ca85230eda01c1f2c
Tomei Alexandru <all3x1995@yahoo.com>
2018-03-17 12:26:17 +0200

    Controller Update

--- SilentWeb/Controllers/ServiceController.cs
+++ SilentWeb/Controllers/ServiceController.cs
@@ -1,39 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace SilentWeb.Controllers
 {
     public class ServiceController : ApiController
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
+        [HttpPost]
+        public void Post([FromBody] XElement value)
         {
-            return new string[] { "value1", "value2" };
+            XmlSerializer serializer = new XmlSerializer(typeof(Test));
+            var test = serializer.Deserialize(value.CreateReader());
         }
 
-        // GET api/<controller>/5
-        public string Get(int id)
+        [XmlRoot("Test")]
+        public class Test
         {
-            return "value";
-        }
-
-        // POST api/<controller>
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
+            [XmlElement("Id")]
+            public int Id { get; set; }
+            [XmlElement("Text")]
+            public string Text { get; set; }
         }
     }
 }
